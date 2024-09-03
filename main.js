5326a9fa0d94b78ca25b51d97925ea17be316c5b
Joaoluisribeir <144291980+Joaoluisribeir@users.noreply.github.com>
2024-09-03 20:30:51 -0300

    Test

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.wa').addEventListener('click', function(event) {
+               event.preventDefault();
+               window.location.href = 'https://wa.link/9x65hj';
+           });
+           document.querySelector('.fe').addEventListener('click', function(event) {
+               event.preventDefault();
+               window.location.href = 'https://www.facebook.com/joaoluis.ribeiro.5811';
+           });
+           document.querySelector('.wa').addEventListener('click', function(event) {
+               event.preventDefault();
+               window.location.href = 'https://www.instagram.com/joaoluis755?igsh=YzljYTk1ODg3Zg==';
+           });
